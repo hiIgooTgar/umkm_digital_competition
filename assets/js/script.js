41a494d28391fa8d0159004ed6c68cab2b914133
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2025-05-26 23:24:38 +0700

    Make add page:Testimonials

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,7 @@ window.addEventListener("scroll", function () {
   let navbarNav = document.querySelector(".navbar");
   navbarNav.classList.toggle("sticky-nav", window.scrollY > 0);
 });
+
+// scroll infinite
+const rowTestimonials = document.querySelector(".row-testimonials");
+rowTestimonials.innerHTML += rowTestimonials.innerHTML;

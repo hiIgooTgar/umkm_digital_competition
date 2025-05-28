eca2b1804f533afa11c2a884fcab12c3dec9fe1e
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2025-05-28 23:20:49 +0700

    Updated section frame:Padding Section, Scroll Info, Scroll X Categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,28 @@ window.addEventListener("scroll", function () {
   navbarNav.classList.toggle("sticky-nav", window.scrollY > 0);
 });
 
+// scroll-up
+window.addEventListener("scroll", function () {
+  let scrollUp = document.querySelector(".scroll-up");
+  scrollUp.classList.toggle("sticky-scroll-up", window.scrollY > 0);
+});
+
 // scroll infinite
 const rowTestimonials = document.querySelector(".row-testimonials");
 rowTestimonials.innerHTML += rowTestimonials.innerHTML;
 
 // copyright
 document.querySelector("#year_copyright").innerHTML = new Date().getFullYear();
+
+// scrollX Categories
+const scrollXCateory = document.getElementById("category-scroll");
+const btnCategoryLeft = document.querySelector(".left-paginate");
+const btnCategoryRight = document.querySelector(".right-paginate");
+
+btnCategoryLeft.addEventListener("click", () => {
+  scrollXCateory.scrollBy({ left: -200, behavior: "smooth" });
+});
+
+btnCategoryRight.addEventListener("click", () => {
+  scrollXCateory.scrollBy({ left: 200, behavior: "smooth" });
+});

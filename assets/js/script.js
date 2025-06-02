4e5aae65df7e632c6e40e41d34fd5632717e7de3
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2025-06-02 22:00:48 +0700

    Updated Responsive All Page and Nav Link Active on Scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,3 +76,28 @@ items.forEach((item) => {
     }
   });
 });
+
+// active menu nav every section
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".nav-list");
+
+function scrollTracker() {
+  const scrollY = window.scrollY;
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 150;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active-menu");
+        if (link.getAttribute("href") === `#${sectionId}`) {
+          link.classList.add("active-menu");
+        }
+      });
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollTracker);
